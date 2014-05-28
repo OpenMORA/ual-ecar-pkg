23f00dea42d3d4e73e99e01e391e17f2729ac3a5
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2014-05-28 12:01:29 +0200

    Fixes to build in Windows

--- Claraquino/main.cpp
+++ Claraquino/main.cpp
@@ -2,8 +2,6 @@
    |                 Open MORA (MObile Robot Arquitecture)                     |
    +---------------------------------------------------------------------------+ */
 
-#include "CClaraquino.h" // User C++ app class 
-
 #define MORA_APP_CLASS         CClaraquino     
 #define MORA_APP_NAME          "Claraquino"    // Default MOOSApp app name
 
